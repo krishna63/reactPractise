fb6bafbb7914372611c1fab38cfa86ea41c1ee4b
unknown <saikrishna63@gmail.com>
2021-01-10 17:13:32 +0530

    feat: basic set up for react application

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+function App() {
+	return (
+		<div>Welcome to reselect and sagassss</div>
+	)
+}
+ 
+render(<App />, document.getElementById('container'));
